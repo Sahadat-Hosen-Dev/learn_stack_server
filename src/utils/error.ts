f21a4d1471d526ff6b4c69842a28647e9482e973
Dev.Topu <devTopu@gmail.com>
2026-06-24 21:19:23 +0300

    create custom error handling file

---
+++ src/utils/error.ts
@@ -0,0 +1,17 @@
+export type CustomError = Error & {
+  code: number;
+  error: string;
+};
+
+function error(
+  code = 500,
+  error: string,
+  msg: string = "Something went wrong",
+): CustomError {
+  const e = new Error(msg) as CustomError;
+  e.code = code;
+  e.error = error;
+  return e;
+}
+
+export default error;
